8b803aa5e909aa7615424315056f1ff2f2b8f221
Konstantinos Mitsides <konstantinos.mitsides23@imperial.ac.uk>
2023-09-11 17:24:03 +0100

    replaced row and col with a tuple called position. Replace target_row and target_col with a tuple called target_position

--- main.py
+++ main.py
@@ -26,17 +26,16 @@ def setup_robot(grid_size):
 	id = rd.randint(0,10000000000000000)
 	row = rd.randint(0,grid_size)
 	col = rd.randint(0,grid_size)
+	position = (row, col)
 	direction = rd.choice(('n', 's', 'e', 'w'))
-	return (name, id, row, col, direction)
+	return (name, id, position, direction)
 
 def print_robot_greeting(name, identifier):
 	print(f'Hello. My name is {name}. My ID is {identifier}.')
 
 def navigate(current_direction,
-             current_row,
-             current_col,
-             target_row,
-             target_col,
+    	     position,
+             target_position,
              grid_size):
 	""" Navigate robot.
 
@@ -56,46 +55,50 @@ def navigate(current_direction,
 		str : Robot's new direction
 	"""
 
-	if current_row == target_row and current_col == target_col:
-		print(f'I am currently at {(target_row, target_col)}!')
+	if position == target_position:
+		print(f'I am currently at {target_position}!')
 		print('I am finally here enjoying my drink!')
 		return None
 	else:
 		if current_direction == "n":
-			print(f'I am currenlty at {(current_row, current_col)}, facing North.')
-			if current_row == 0:
+			print(f'I am currenlty at {position}, facing North.')
+			if position[0] == 0:
 				print('There is a wall in front of me so I am rotating 90 degrees clockwise!')
-				return (current_row, current_col, 'e')
+				return (position, 'e')
 			else:
+				position = (position[0] - 1, position[1])
 				print('Moving forward')
-				return (current_row - 1, current_col, current_direction)
+				return (position, current_direction)
 
 		elif current_direction == "s":
-			print(f'I am currenlty at {(current_row, current_col)}, facing South.')
-			if current_row == grid_size - 1:
+			print(f'I am currenlty at {position}, facing South.')
+			if position[0] == grid_size - 1:
 				print('There is a wall in front of me so I am rotating 90 degrees clockwise!')
-				return (current_row, current_col, 'w')
+				return (position, 'w')
 			else:
+				position = (position[0] + 1, position[1])
 				print('Moving forward')
-				return (current_row + 1, current_col, current_direction)
+				return (position, current_direction)
 
 		elif current_direction == "e":
-			print(f'I am currenlty at {(current_row, current_col)}, facing East.')
-			if current_col == grid_size - 1:
+			print(f'I am currenlty at {position}, facing East.')
+			if position[1] == grid_size - 1:
 				print('There is a wall in front of me so I am rotating 90 degrees clockwise!')
-				return (current_row, current_col, 's')
+				return (position, 's')
 			else:
+				position = (position[0], position[1] + 1)
 				print('Moving forward')
-				return (current_row, current_col + 1, current_direction)
+				return (position, current_direction)
 
 		else:
-			print(f'I am currenlty at {(current_row, current_col)}, facing West.')
-			if current_col == 0:
+			print(f'I am currenlty at {position}, facing West.')
+			if position[1] == 0:
 				print('There is a wall in front of me so I am rotating 90 degrees clockwise!')
-				return (current_row, current_col, 'n')
+				return (position, 'n')
 			else:
+				position = (position[0], position[1] - 1)
 				print('Moving forward')
-				return (current_row, current_col - 1, current_direction)
+				return (position, current_direction)
 		
 			
 
@@ -103,7 +106,7 @@ def navigate(current_direction,
 
 
 
-def run_simulation(grid_size=10, target_row=9, target_col=9):
+def run_simulation(grid_size=10):
 	""" Start robot navigation simulation.
 		
 	Args:
@@ -118,12 +121,12 @@ def run_simulation(grid_size=10, target_row=9, target_col=9):
 	print()
 	for i in range(3):
 		target_list = [0,grid_size - 1]
-		target_row, target_col = rd.choice(target_list), rd.choice(target_list)
+		target_position = (rd.choice(target_list), rd.choice(target_list))
 		print(f'{l[i][0]} is searching for its drink!')
-		ans = navigate(l[i][4], l[i][2], l[i][3], target_row, target_col, grid_size)
+		ans = navigate(l[i][3], l[i][2], target_position, grid_size)
 		while ans is not None:
-			row, col, direction = ans
-			ans = navigate(direction, row, col, target_row, target_col, grid_size)
+			position, direction = ans
+			ans = navigate(direction, position, target_position, grid_size)
 		print()
 	
 	return None
